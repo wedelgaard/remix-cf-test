f523843bb86aeae9cbdfabcdd24874b543409607
Michael Falck Wedelgård <michael@wedelgaard.dk>
2022-08-10 09:37:08 +0200

    Setup env from remix docs

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { LoaderFunction, MetaFunction } from "@remix-run/cloudflare";
+import {json} from '@remix-run/cloudflare'
 import {
   Links,
   LiveReload,
@@ -6,6 +7,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 export const meta: MetaFunction = () => ({
@@ -14,7 +16,19 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const loader: LoaderFunction = async ({ context }) => {
+  return json({
+    ENV: {
+      HELLO: process.env.HELLO,
+      WORLD: process.env.WORLD,
+    },
+  }); 
+};
+
 export default function App() {
+
+  const data = useLoaderData()
+
   return (
     <html lang="en">
       <head>
@@ -26,6 +40,13 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(
+              data.ENV
+            )}`,
+          }}
+        />
       </body>
     </html>
   );
